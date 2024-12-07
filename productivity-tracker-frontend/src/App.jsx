0f157a45980afacadc7b39ca0af0152ffad26248
Sri Durgesh <60750509+sridurgeshv@users.noreply.github.com>
2024-12-07 15:10:01 +0000

    adding the css

--- productivity-tracker-frontend/src/App.jsx
+++ productivity-tracker-frontend/src/App.jsx
@@ -5,20 +5,19 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <Link to="/productivity">Go to Productivity Tracker</Link>
-        </nav>
-        
+      <div>        
         <Routes>
           <Route path="/productivity" element={<ProductivityTracker />} />
           <Route path="/" element={
             <div>
-              <h1>Welcome to the Home Page</h1>
-              <p>Click the link to go to Productivity Tracker</p>
+              <h1>Welcome Home, Users</h1>
+              <p>Are you overwhelmed by juggling multiple tasks and searching for a powerful productivity tool? Look no further! Click the above button to discover our cutting-edge Productivity Tracker, designed to streamline your workflow and elevate your efficiency to new heights.</p>
             </div>
           } />
         </Routes>
+        <nav>
+          <Link to="/productivity">Let's Go</Link>
+        </nav>
       </div>
     </Router>
   )
