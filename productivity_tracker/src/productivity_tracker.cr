07965757b98f9f60d4506f964584fe91e6ce09ba
Sri Durgesh <60750509+sridurgeshv@users.noreply.github.com>
2024-12-07 11:03:15 +0000

    Made few changes

--- productivity_tracker/src/productivity_tracker.cr
+++ productivity_tracker/src/productivity_tracker.cr
@@ -7,4 +7,12 @@ module ProductivityTracker
   TaskController.setup_routes
 
   Kemal.run
+
+  before_all do |env|
+    env.response.headers["Access-Control-Allow-Origin"] = "*"
+    env.response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    env.response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+
+end
+
 end
